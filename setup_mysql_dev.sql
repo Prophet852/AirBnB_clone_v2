8f86fee8b01a5e6d9c60d21eabd529915e42848a
Prophet852 <56918019+Prophet852@users.noreply.github.com>
2024-04-01 21:05:45 +0200

    Update setup_mysql_dev.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,6 +1,5 @@
 -- Prepares a MySQL server for the project.
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-<<<<<<< HEAD
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
@@ -9,8 +8,3 @@ GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
 GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
-=======
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema . * TO 'hbnb_dev'@'localhost';
->>>>>>> 8833106e1678c80de7d1f0b104ab99e17a0bbef6
