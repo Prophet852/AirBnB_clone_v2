bf54c7a3118b478c23b9958d859b6b7ad78bb2c6
Prophet852 <dingawillie@outlook.com>
2024-04-02 07:37:26 -0700

    fix Airbnb v2

--- console.py
+++ console.py
@@ -113,34 +113,6 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-<<<<<<< HEAD
-    def do_create(self, line):
-        """ Create an object of any class"""
-        try:
-            if not line:
-                raise SyntaxError()
-            my_list = line.split(" ")
-            kwargs = {}
-            for i in range(1, len(my_list)):
-                key, value = tuple(my_list[i].split("="))
-                if value[0] == '"':
-                    value = value.strip('"').replace("_", " ")
-                else:
-                    try:
-                        value = eval(value)
-                    except (SyntaxError, NameError):
-                        continue
-                    kwargs[key] = value
-
-                if kwargs == {}:
-                     obj = eval(my_list[0])()
-                else:
-                    obj = eval(my_list[0])(**kwargs)
-                    storage.new(obj)
-                print(obj.id)
-                obj.save()
-                
-=======
     def do_create(self, args):
         """ Create an object of any class"""
         try:
@@ -154,7 +126,6 @@ class HBNBCommand(cmd.Cmd):
                 if type(arg_splited[1]) is str:
                     arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')
                 kw[arg_splited[0]] = arg_splited[1]
->>>>>>> 8833106e1678c80de7d1f0b104ab99e17a0bbef6
         except SyntaxError:
             print("** class name missing **")
         except NameError:
